f1829367a058d352dc7206865cc21b646ea9584d
Svetlana Okladnova <sokladnova@yandex.ru>
2025-02-22 19:54:57 +0300

    Изменены данные для регистрации

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
 class Data:
     STELLAR_BURGERS_URL = "https://stellarburgers.nomoreparties.site/"
 
-    VALID_REGISTER_NAME = "Ана"
-    VALID_REGISTER_EMAIL = "anytasu565634345365726per@gmail.com"
+    VALID_REGISTER_NAME = "Светлана"
+    VALID_REGISTER_EMAIL = "svetlana_okladnova_18_200@yandex.ru"
     VALID_REGISTER_PASSWORD = "235445345213"
 
     INVALID_REGISTER_PASSWORD = "1"
